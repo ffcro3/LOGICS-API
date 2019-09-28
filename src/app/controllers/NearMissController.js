c6bc0a8466c6620b76cfe237d5da433f9c8dcced
Fabricio Rocha <fabriciofrocha87@gmail.com>
2019-09-28 18:55:18 -0300

    Near Miss Controller for exporting data

--- src/app/controllers/NearMissController.js
+++ src/app/controllers/NearMissController.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable no-await-in-loop */
 /* eslint-disable prefer-destructuring */
 /* eslint-disable no-plusplus */
@@ -9,80 +10,85 @@ import api from '../../config/api';
 
 class NearMissController {
   /**
-   * THIS ROUTE RECORDS ALL NEAR MISSES FROM LOGICS
+   * THIS ROUTE RECORDS ALL MASTER INCIDENT FROM LOGICS
    */
   async index(req, res) {
     const { year } = req.params;
+    let logicsAllURL = '';
+    let logicsCountAll = '';
+    let writeStream = '';
+
+    console.log('Near Miss Route accessed');
 
     if (year) {
-      console.log('Route accessed');
-
       const fullYear = `${year}-01-01T00:00:00Z`;
 
-      const logicsAllURL = `${process.env.NEARMISS_DATA}&$filter=DateaTimeOccur ge ${fullYear}`;
-      const logicsCountAll = `${process.env.NEARMISS_COUNT}&$filter=DateaTimeOccur ge ${fullYear}`;
-
-      const logicsData = await api
-        .get(logicsCountAll)
-        .then(response => response);
-
-      const NearmissQuantitity = logicsData.data['@odata.count'];
-
-      // Get BBS pages based into 500 records limit by page
-      const pages = Math.round(NearmissQuantitity / 500);
-
-      console.log(`You have ${pages} to extract...`);
-      console.log(`Beggining...`);
-
-      return res.status(200).json({
-        data: fullYear,
-        URL: logicsAllURL,
-        Quantity: NearmissQuantitity,
-      });
+      logicsAllURL = `${process.env.NEARMISS_DATA}&$filter=DateofOccurence ge ${fullYear} and Deleted eq false and IncidTypeHelper eq 'Near Miss / Hazard Report'`;
+      logicsCountAll = `${process.env.NEARMISS_COUNT}&$filter=DateofOccurence ge ${fullYear} and Deleted eq false and IncidTypeHelper eq 'Near Miss / Hazard Report'`;
+    }
+    if (!year) {
+      logicsCountAll = `${process.env.NEARMISS_COUNT}&$filter=Deleted eq false and IncidTypeHelper eq 'Near Miss / Hazard Report'`;
+      logicsAllURL = `${process.env.NEARMISS_DATA}&$filter=Deleted eq false and IncidTypeHelper eq 'Near Miss / Hazard Report'`;
     }
 
-    let start_time = new Date().getTime();
+    const logicsData = await api.get(logicsCountAll).then(response => response);
 
-    const logicsData = await api
-      .get(process.env.NEARMISS_COUNT)
-      .then(response => response);
-
-    const NearmissQuantitity = logicsData.data['@odata.count'];
+    const NearMissQuantity = logicsData.data['@odata.count'];
 
     // Get BBS pages based into 500 records limit by page
-    const pages = Math.round(NearmissQuantitity / 500);
+    const pages = Math.round(NearMissQuantity / 500);
 
     console.log(`You have ${pages} to extract...`);
     console.log(`Beggining...`);
 
     // For loop to record every single page listed
     for (let index = 0; index <= pages; index++) {
-      const paginationData = await api
-        .get(process.env.NEARMISS_DATA + Number(index) * 500)
-        .then(response => response);
+      try {
+        const paginationData = await api
+          .get(`${logicsAllURL}&$skip=${Number(index) * 500}`)
+          .then(response => response);
 
-      // starts the stream
-      const writeStream = fs.createWriteStream(
-        `${resolve(
-          __dirname,
-          '..',
-          '..',
-          '..',
-          'tmp',
-          'NearMisses'
-        )}/nearmiss-${index}.json`
-      );
-      // write near miss data
-      writeStream.write(JSON.stringify(paginationData.data.value, null));
+        if (year) {
+          // starts the stream
+          writeStream = fs.createWriteStream(
+            `${resolve(
+              __dirname,
+              '..',
+              '..',
+              '..',
+              'tmp',
+              'NearMiss',
+              year
+            )}/nearmiss-${index}.txt`
+          );
+        }
+        if (!year) {
+          // starts the stream
+          writeStream = fs.createWriteStream(
+            `${resolve(
+              __dirname,
+              '..',
+              '..',
+              '..',
+              'tmp',
+              'NearMiss',
+              'All'
+            )}/nearmiss-${index}.txt`
+          );
+        }
+        // write master incident data
+        writeStream.write(JSON.stringify(paginationData.data.value, null));
 
-      // the finish event is emitted when all data has been flushed from the stream
-      writeStream.on('finish', () => {
-        console.log(`page ${index} was wrote`);
-        console.log('Time elapsed:', (new Date().getTime() - start_time) / 60);
-      });
+        // the finish event is emitted when all data has been flushed from the stream
+        writeStream.on('finish', () => {
+          console.log(`page ${index} was wrote`);
+        });
 
-      // close the stream
-      writeStream.end();
+        // close the stream
+        writeStream.end();
+      } catch (err) {
+        console.log(err);
+      }
     }
 
     return res.send('Finished');
